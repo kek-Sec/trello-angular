74c8db552ec5775edba02ef5707dd4c77bc255e2
kek-Sec <georgepetrakis99@gmail.com>
2020-12-11 02:10:00 +0200

    home is working,added bootstrap

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -13,6 +13,8 @@ export class LandingComponent implements OnInit {
   Images:Array<String> = new Array<String>(10);
   Text:Array<String> = new Array<String>(10);
   URLS:Array<String>= new Array<String>(10);
+  Dates:Array<String>= new Array<String>(10);
+
 
   constructor(private trelloService:TrelloService) { }
 
@@ -25,7 +27,34 @@ export class LandingComponent implements OnInit {
     this.trelloService.getCards().subscribe(data => {
       this.CARDS = data;
       console.log(data);
+      this.createArrays(this.CARDS);
     })
   }
 
+  getImages(index,card_id,att_id)
+  {
+    console.log(att_id);
+    this.trelloService.getAttachment(card_id,att_id).subscribe(data => {
+      
+      this.Images[index] = data['url'];
+    })
+  }
+
+  createArrays(cards:JSON)
+  {
+    let i = 0;
+    for(let article in cards)
+    {
+      //limit to 10 quotes to avoid pagination
+      if(i >= 10) {break;}
+
+      this.Titles[article] = cards[article]['name'];
+      this.Dates[article] = cards[article]['dateLastActivity'];
+      this.Text[article] = cards[article]['desc'];
+      this.URLS[article] = cards[article]['shortUrl'];
+      this.getImages(i,cards[article]['id'],cards[article]['idAttachmentCover']);
+      i++;
+      //this.Titles[article] = news_items['articles'][article]['title'];
+    }
+  }
 }
