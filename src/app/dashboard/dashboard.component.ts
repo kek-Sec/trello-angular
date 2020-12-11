aaae7ec8204c995ef77e5bf071737450e656df7d
kek-Sec <georgepetrakis99@gmail.com>
2020-12-11 02:52:59 +0200

    added update and delete to service

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import { TrelloService } from '../trello.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  CARDS: JSON;
+  Titles: Array<String> = new Array<String>(10);
+  ID: Array<String> = new Array<String>(10);
+  Images: Array<String> = new Array<String>(10);
+  Text: Array<String> = new Array<String>(10);
+  URLS: Array<String> = new Array<String>(10);
+  Dates: Array<String> = new Array<String>(10);
+
+  constructor(private trelloService: TrelloService) {}
 
   ngOnInit(): void {
+    this.getQuotes();
   }
 
+  getQuotes()
+  {
+    this.trelloService.getCards().subscribe(data => {
+      this.CARDS = data;
+      console.log(data);
+      this.createArrays(this.CARDS);
+    })
+  }
+
+ getImages(index,card_id,att_id)
+  {
+    console.log(att_id);
+    this.trelloService.getAttachment(card_id,att_id).subscribe(data => {
+      
+      this.Images[index] = data['url'];
+    })
+  }
+
+  createArrays(cards:JSON)
+  {
+    let i = 0;
+    for(let article in cards)
+    {
+      //limit to 10 quotes to avoid pagination
+      if(i >= 10) {break;}
+
+      this.Titles[article] = cards[article]['name'];
+      this.Dates[article] = cards[article]['dateLastActivity'];
+      this.Text[article] = cards[article]['desc'];
+      this.URLS[article] = cards[article]['shortUrl'];
+      this.getImages(i,cards[article]['id'],cards[article]['idAttachmentCover']);
+      this.ID[article] = cards[article]['id'];
+      i++;
+      //this.Titles[article] = news_items['articles'][article]['title'];
+    }
+  }
+
+  edit(ID){
+    console.log(ID);
+    
+  }
+  delete(ID)
+  {
+    this.trelloService.deleteCard(ID).subscribe(data => {
+      console.log(data);
+    })
+  }
+  add()
+  {
+
+  }
 }
--- src/app/trello.service.ts
+++ src/app/trello.service.ts
@@ -38,4 +38,25 @@ export class TrelloService {
       .get<JSON>(url)
       .pipe(tap((_) => console.log(`got attachment!`)));
   }
-}
+
+  deleteCard(card_id): Observable<JSON> {
+    const url =
+      'https://api.trello.com/1/cards/' +
+      card_id +
+      '/?key=90ec957c4e8cdf8ace5677a945ff1d21&token=85d4cc5d590944feb7cb2d79327514a74e110fefa5bbddbb7d25348c848a7c79';
+      return this.http.delete<JSON>(url).pipe(tap((_) => console.log('deleted card!')));
+  }
+
+  updateCard(Author, Quote, Image_url,ID): Observable<JSON> {
+    const url =
+      'https://api.trello.com/1/cards/' + ID + '?key=90ec957c4e8cdf8ace5677a945ff1d21&token=85d4cc5d590944feb7cb2d79327514a74e110fefa5bbddbb7d25348c848a7c79&idList=5fd2a994b357a23260e2b83c&name=' +
+      Author +
+      '&desc=' +
+      Quote +
+      '&urlSource=' +
+      Image_url;
+
+    return this.http
+      .put<JSON>(url, null)
+      .pipe(tap((_) => console.log('posted card!')));
+  }}
